85c47ae994701ba89ba328198072a60410691efa
Isa Mary <isa.kusuki@gmail.com>
2023-04-07 19:48:16 -0300

    Criação do projeto

---
+++ src/main/java/br/com/TalentTracker/TalentTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.TalentTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/TalentTracker/TalentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.TalentTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

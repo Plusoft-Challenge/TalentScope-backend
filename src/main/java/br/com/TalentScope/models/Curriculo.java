91d79ef38e1760f4ff0bef191ff60a7ec7f856af
Isa Mary <isa.kusuki@gmail.com>
2023-05-20 14:09:57 -0300

    Relacionamentos

--- src/main/java/br/com/TalentScope/models/Curriculo.java
+++ src/main/java/br/com/TalentScope/models/Curriculo.java
@@ -1,19 +1,29 @@
 package br.com.TalentScope.models;
 
-import java.sql.Blob;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(of = "id")
 public class Curriculo {
 
 	@Id
@@ -32,60 +42,8 @@ public class Curriculo {
 
 	@NotNull
 	private String email;
-
-	// private Feedback feedback;
-	// private Vaga vaga;
-
-	public Curriculo() {
-	}
-
-	public Curriculo(String arquivo, LocalDateTime dtEnvioCurriculo, String nomeCandidato,
-			String email) {
-		super();
-		this.arquivo = arquivo;
-		this.dtEnvioCurriculo = dtEnvioCurriculo;
-		this.nomeCandidato = nomeCandidato;
-		this.email = email;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getArquivo() {
-		return arquivo;
-	}
-
-	public void setArquivo(String arquivo) {
-		this.arquivo = arquivo;
-	}
-
-	public LocalDateTime getDtEnvioCurriculo() {
-		return dtEnvioCurriculo;
-	}
-
-	public void setDtEnvioCurriculo(LocalDateTime dtEnvioCurriculo) {
-		this.dtEnvioCurriculo = dtEnvioCurriculo;
-	}
-
-	public String getNomeCandidato() {
-		return nomeCandidato;
-	}
-
-	public void setNomeCandidato(String nomeCandidato) {
-		this.nomeCandidato = nomeCandidato;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
+	@ManyToOne
+	private Vaga vaga;
 
 }

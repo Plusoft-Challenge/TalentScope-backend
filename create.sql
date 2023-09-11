e7a5c4a9a43dbf5bbd0ebf993be68af2c5067d11
Isa Mary <isa.kusuki@gmail.com>
2023-09-11 19:28:27 -0300

    Configuração SQL Oracle

---
+++ create.sql
@@ -0,0 +1,12 @@
+create table curriculo (id number(19,0) generated as identity, arquivo blob, dt_envio_curriculo timestamp not null, email varchar2(255 char) not null, nome_candidato varchar2(255 char) not null, ranking number(10,0) not null, vaga_id number(19,0), primary key (id));
+create table feedback (id number(19,0) generated as identity, aprovado number(1,0), dt_analise timestamp not null, dt_envio_feedback timestamp, envio_feedback number(1,0) not null, feedback varchar2(255 char) not null, primary key (id));
+create table habilidade (id number(19,0) generated as identity, nome varchar2(255 char) not null, primary key (id));
+create table nivel_permissao (id number(19,0) generated as identity, descricao varchar2(255 char) not null, nome varchar2(255 char) not null, primary key (id));
+create table usuario (id number(19,0) generated as identity, dt_alteracao timestamp, dt_criacao timestamp not null, email varchar2(255 char) not null, nome varchar2(80 char) not null, senha varchar2(16 char) not null, status number(1,0) not null, nivel_permissao_id number(19,0), primary key (id));
+create table vaga (id number(19,0) generated as identity, descricao_cargo varchar2(255 char), descricao_vaga varchar2(1000 char), dt_abertura timestamp not null, dt_encerramento timestamp not null, nome varchar2(255 char), usuario_id number(19,0), primary key (id));
+create table vaga_habilidades (vaga_id number(19,0) not null, habilidades_id number(19,0) not null);
+alter table curriculo add constraint fk_curriculo_vaga foreign key (vaga_id) references vaga;
+alter table usuario add constraint fk_usuario_permissao foreign key (nivel_permissao_id) references nivel_permissao;
+alter table vaga add constraint fk_vaga_usuario foreign key (usuario_id) references usuario;
+alter table vaga_habilidades add constraint fk_habilidades_habilidade foreign key (habilidades_id) references habilidade;
+alter table vaga_habilidades add constraint fk_vagas_vaga foreign key (vaga_id) references vaga;
